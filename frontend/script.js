4b4deb84ca00d2923ee17650a5579cb58598ef91
Neptun Bozkurt <neptunb@gmail.com>
2024-12-03 22:10:03 +0300

    Synch Callbacks

--- frontend/script.js
+++ frontend/script.js
@@ -8,36 +8,46 @@ const print = (msg) => {
   document.body.appendChild(newDiv);
 } 
 
-// Event Loop Examples
-console.log('Start');
-print('Start')
-Promise.resolve().then(() => {
-  console.log('Promise');
-  print('Promise')
+/* ************************************************* Synchronous Callbacks *****  */
+
+// function myCallback(){
+//   console.log("Step-myCallback");
+//   print("Step-myCallback")
+// }
+
+// function mainFunc(cb) {
+//   console.log("Step-Main");
+//   print("Step-Main")
+//   if (typeof cb == 'function'){
+//     cb();
+//   }
+// }
+
+// mainFunc(myCallback);
+
+
+
+const numbers = [1, 2, 3, 4, 5];
+
+// Doubled numbers with map
+print("\n***** doubled numbers in map *****");
+const doubled = numbers.map(function(number) {
+  return number * 2;
 });
-console.log('End');
-print('End')
+console.log(doubled);
+print(doubled)
 
+// Even numbers with filter
+print("\n***** even numbers in filter *****");
+const evens = numbers.filter(function(number) {
+  return number % 2 === 0;
+});
+console.log(evens); 
+print(evens)
 
-// console.log('Start');
-// print('Start')
-// setTimeout(() => {
-//   console.log('Timeout');
-//   print('Timeout')
-// }, 1000);
-// console.log('End');
-// print('End')
-
-
-
-
-// // What is IIFE (Immediately Invoked Function Expression)
-// (async() => {
-//   console.log('Start');print('Start')
-//   await Promise.resolve().then(() => {
-//     console.log('Promise');print('Promise')
-//   });
-//   console.log('End');print('End')
-// })();
-
-/* Reference:  http://latentflip.com/loupe */
+// Log numbers with forEach
+print("\n***** numbers in forEach *****");
+numbers.forEach(function(number) {
+  console.log(number);
+  print(number)
+});
