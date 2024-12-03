3fa9cf13c24d72b1e443f5c2a23a91a3956f44cb
Neptun Bozkurt <neptunb@gmail.com>
2024-12-03 16:32:20 +0300

    Timers

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,43 @@
-document.getElementById('fetchData').addEventListener('click', async () => {
-  const response = await fetch('/api');
-  const data = await response.json();
-  document.getElementById('response').innerText = data.message;
+let flip_flop = ""
+const setTimeoutFunc = (ms) => {
+  setTimeout(() => {
+    flip_flop = flip_flop === "UP"? "DOWN":"UP";
+    document.getElementById('timeout_response').innerText = flip_flop + " after 2000 miliseconds";
+  }, ms);
+}
+
+const setImmediateFunc = () => {
+  try {
+    setImmediate(() => {
+      document.getElementById('immediate_response').innerText = "setImmediate applied.";
+    });
+  } catch (error) {
+    document.getElementById('immediate_response').innerText = "setImmediate runs only at node environments.";
+  }
+  
+}
+
+let counter = 0
+const setIntervalFunc = (ms) => {
+  setInterval(() => {
+    counter = counter +1;
+    document.getElementById('interval_response').innerText = "setInterval applied: " + counter;
+  }, ms);
+}
+
+
+document.getElementById('timeout').addEventListener('click', async () => {
+  document.getElementById('timeout_response').innerText = "setTimeout started.";
+  setTimeoutFunc(2000);
+});
+
+document.getElementById('immediate').addEventListener('click', async () => {
+  document.getElementById('immediate_response').innerText = "";
+  setImmediateFunc();
+});
+
+document.getElementById('interval').addEventListener('click', async () => {
+  counter = 0;
+  document.getElementById('interval_response').innerText = "setInterval started.";
+  setIntervalFunc(1000);
 });
